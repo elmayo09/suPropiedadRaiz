77d67d36e4a73f6684109d528b659653e933368c
juangh15 <47037988+juangh15@users.noreply.github.com>
2019-02-03 14:58:02 -0500

    tostring compraventa

--- Compraventa.py
+++ Compraventa.py
@@ -47,6 +47,10 @@ class Compraventa:
     def getInmbueble(self):
         return self._inmueble
 
+    def toString(self):
+        printer = "{"+"codigo: "+str(self._codigo)+", fecha: "+str(self._fecha)+", valor: "+str(self._valor)+", fechafin: "+str(self._fechafin)+", inmueble: "+str(self._inmueble)+", propietario: "+str(self._propietario)+", comprador: "+str(self._comprador)+" }"
+        return printer
+    
     @staticmethod
     def sinComprador(compraventas):
         lista=[]
