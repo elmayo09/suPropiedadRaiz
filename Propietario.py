0dfc248861728dc47bdfe423fc70a659e9ee74f2
juangh15 <47037988+juangh15@users.noreply.github.com>
2019-02-04 11:19:04 -0500

    propietario añadido

---
+++ Propietario.py
@@ -0,0 +1,28 @@
+from Usuario import Usuario
+class Propietario(Usuario):
+    def __init__(self,cedula,nombre, contrasena,direccion,correo="No"):
+        super().__init__(cedula,nombre,contrasena,correo)
+        self._direccion=direccion
+        self._inmuebles = []
+        self._funcionario=None
+
+
+    def toString(self):
+        printer = "Propietario: {"+self._nombre+", "+self._direccion+", "+self._correo+", "+str(self._cedula)+" }"
+        return printer
+    
+    
+    def getDireccion(self):
+        return self._direccion
+    def setDireccion(self,direccion):
+        self._direccion=direccion
+    def getInmuebles(self):
+        return self._inmuebles
+    def addInmueble(self, nuevoInmueble):
+        self._inmuebles.append(nuevoArriendo)
+    def setFuncionario(self,funcionario):
+        self._funcionario=funcionario
+    def getFuncionario(self):
+        return self._funcionario
+
+    
