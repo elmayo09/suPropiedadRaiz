905cb51d6e6d356b065cee252a79aa3946bc5e21
elmayo09 <43676331+elmayo09@users.noreply.github.com>
2019-02-14 17:26:08 -0500

    Add files via upload

--- Cliente.py
+++ Cliente.py
@@ -1,11 +1,11 @@
 
-from Usuario import Usuario
-class Cliente(Usuario):
+class Cliente:
     def __init__(self,cedula,nombre, contrasena,direccion,correo="No"):
-        super().__init__(cedula,nombre,contrasena,correo)
+        self._cedula=cedula
+        self._nombre=nombre
+        self._correo=correo
+        self._contrasena=contrasena
         self._direccion=direccion
-        #self._arriendos=[]
-        #self._compraventas=[]
         self._contratos=[]
 
     def __str__(self):
@@ -16,18 +16,34 @@ class Cliente(Usuario):
         return self._direccion
     def setDireccion(self,direccion):
         self._direccion=direccion
-    #def getArriendos(self):
-     #   return self._arriendos
-    #def addArriendo(self, nuevoArriendo):
-     #   self._arriendos.append(nuevoArriendo)
-    #def getCompraventas(self):
-     #   return self._compraventas
-    #def addCompraventa(self, nuevaCompraventa):
-     #   self._compraventas.append(nuevaCompraventa)
+    def getNombre(self):
+        return self._nombre
+    def setNombre(self, nombre):
+        self._nombre=nombre
+    def getContrasena(self):
+        return self._contrasena
+    def setContrasena(self, contra):
+        self._contrasena=contra
+    def getCedula(self):
+        return self._cedula
+    def setCedula(self, cedula):
+        self._cedula=cedula
+    def getCorreo(self):
+        return self._correo
+    def setCorreo(self, correo):
+        self._correo=correo
     def addContrato(self, contrato):
         self._contratos.append(contrato)
     @staticmethod
     def mostrarClientes(lista):
         for u in lista:
             print(u.toString())
-
+    @staticmethod
+    def login(ced, contra, lista):
+        for x in lista:
+            if x.getCedula()==ced:              
+                if x.getContrasena()==contra:
+                    return x
+                else:
+                    return None
+        return None
