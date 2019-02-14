6b13950726887790920e0107258ee3778db1e73b
elmayo09 <43676331+elmayo09@users.noreply.github.com>
2019-02-14 17:25:23 -0500

    Add files via upload

--- Propietario.py
+++ Propietario.py
@@ -1,26 +1,45 @@
-
-from Usuario import Usuario
-class Propietario(Usuario):
+class Propietario:
     def __init__(self,cedula,nombre, contrasena,direccion,correo="No"):
-        super().__init__(cedula,nombre,contrasena,correo)
+        self._cedula=cedula
+        self._nombre=nombre
+        self._correo=correo
+        self._contrasena=contrasena
         self._direccion=direccion
-        self._compraventas=[]
-        self._arriendos=[]
+        self._inmuebles=[]
 
-    def toString(self):
+    def __str__(self):
         printer = "Propietario: {"+self._nombre+", "+self._direccion+", "+self._correo+", "+str(self._cedula)+" }"
         return printer
     
-    
+    def getNombre(self):
+        return self._nombre
+    def setNombre(self, nombre):
+        self._nombre=nombre
+    def getContrasena(self):
+        return self._contrasena
+    def setContrasena(self, contra):
+        self._contrasena=contra
+    def getCedula(self):
+        return self._cedula
+    def setCedula(self, cedula):
+        self._cedula=cedula
+    def getCorreo(self):
+        return self._correo
+    def setCorreo(self, correo):
+        self._correo=correo
     def getDireccion(self):
         return self._direccion
     def setDireccion(self,direccion):
         self._direccion=direccion
-    def getListaCompraventas(self):
-        return self._compraventas
-    def addCompraventa(self,contrato):
-        self._arriendos.append(contrato)
-    def getListaArriendos(self):
-        return self._arriendos
-    def addArriendo(self,contrato):
-        self._arriendos.append(contrato)
+    def addInmueble(self,inmueble):
+        self._inmuebles.append(inmueble)
+
+    @staticmethod
+    def login(ced, contra, lista):
+        for x in lista:
+            if x.getCedula()==ced:              
+                if x.getContrasena()==contra:
+                    return x
+                else:
+                    return None
+        return None
