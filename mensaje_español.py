69ce33cc9d9b4c59bd2743ce37cbdc6d5d283776
juangh15 <47037988+juangh15@users.noreply.github.com>
2019-02-16 23:11:16 -0500

    mensajes separados por idioma

---
+++ mensaje_español.py
@@ -0,0 +1,35 @@
+
+class Mensaje:
+    title = "---------SuPropiedadRaiz-----------\n"
+    bienv = "*Welcome, select the language / Bienvenido, seleccione el idioma:*"
+    gracias = "Gracias por visitarnos."
+    en_es = "1. English. \n2. Español"
+    err = "Wrong option, try again / Opcion incorrecta, intente de nuevo"
+
+    menu_principal = "1. Agregar datos ficticios.\n2. Ingresar como propietario.\n3. Registrar cliente.\n4. Ingresar cliente.\n0. Salir.\n"
+    ingreso = "Para entrar, ingrese los siguientes datos:\n"
+    registro = "Para registrarse, ingrese los siguientes datos:\n"
+    err_datos = "Datos incorrectos, intente de nuevo"
+    existe = "Ya existe este dato, intentelo de nuevo con uno diferente."
+
+    bienv_usuario = "Bienvenido, "
+    datosFicticios= "Datos ficticios agregados correctamente."
+    seleccion = "----------------------------------------------------\n Ingrese el numero de una de las siguientes opciones: \n"
+    menu_propietario = " 1. Registrar contrato. \n 2. Ver funcionario con mayor sueldo. \n 3. Ver listado de funcionarios. \n 4. Registrar nueva compraventa o arriendo. \n 0. Regresar al menú principal. \n " 
+
+    menu_cliente = " 1. Ver ofertas de arrendamiento. \n 2. Ver ofertas de compraventa. \n 0. Regresar al menú principal"
+    arriendos_disp = "Los arrendamientos disponibles son: \n"
+    compraventas_disp = "Las compraventas disponibles son: \n"
+
+    in_nombre = "Ingrese el nombre: "
+    in_cedula = "Ingrese la cédula: "
+    in_correo = "Ingrese el correo: "
+    opcional_correo = "Desea agregar un correo? :\n1. Si\n2. No"
+    in_contrasena = "Ingrese la contraseña: "
+    in_sueldo = "Ingrese el salario: "
+    in_comision = "Ingrese la comisión: "
+
+    in_direccion = "Ingrese la direccion: "
+    agg = "Agregado correctamente.  \n"
+    regd = "Registrado correctamente.  \n"
+
