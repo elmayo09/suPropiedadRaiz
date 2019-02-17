0938956096224524052c1b4a30b9f0f41ee5e18f
Cristian Tamayo <ctamayoo@unal.edu.co>
2019-02-17 12:37:12 -0500

    registro propietario

--- mensaje_ingles.py
+++ mensaje_ingles.py
@@ -6,7 +6,7 @@ class Mensaje:
     en_es = "English"
     err = "Wrong option, try again "
 
-    menu_principal = " 1. Add fake data.\n 2. Log In as owner. \n 3. Sign In as Costumer. \n 4. Log In as Costumer. \n 0. Exit. \n "
+    menu_principal = " 1. Add fake data.\n 2. Log In as owner. \n 3. Sign In as Costumer. \n 4. Log In as Costumer.\n 5. Register new owner \n 0. Exit. \n "
     ingreso = "For Log In , enter the following data:\n"
     registro = "For Sign In, enter the following data:\n"
     err_datos = "Wrong data, try again"
