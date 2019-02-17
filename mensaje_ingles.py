1b3ed4a21f93bf92944179e638fb50867b5abaf2
Cristian Tamayo <ctamayoo@unal.edu.co>
2019-02-17 10:05:38 -0500

    menu cliente

--- mensaje_ingles.py
+++ mensaje_ingles.py
@@ -17,7 +17,7 @@ class Mensaje:
     seleccion = "----------------------------------------------------\n Enter the number of one of the following options: \n"
     menu_propietario = " 1. Register contract. \n 2. See list of properties. \n 0. Return to main menu. \n "
 
-    menu_cliente = " 1. See rent offers. \n 2. See sales offers. \n 3. Select sale 4. Select rent\n 0. Return to main menu"
+    menu_cliente = " 1. See rent offers. \n 2. See sales offers. \n 3. Select sale \n 4. Select rent \n 5. Show my contracts \n 0. Return to main menu"
     arriendos_disp = "The available rent offers are: \n"
     compraventas_disp = "The available sale offers are: \n"
     sel_compraventa = "Enter the code of the sale: \n"
