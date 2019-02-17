3175fd187f98209bc59fa3380fb6aff95244d1d7
Cristian Tamayo <ctamayoo@unal.edu.co>
2019-02-17 10:10:09 -0500

    menu cliente

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from Compraventa import Compraventa
 from Inmueble import Inmueble
 from Propietario import Propietario
 from Ficticios import Ficticios
+from Contrato import Contrato
 from mensaje_espanol import Mensaje as msg
 from random import *
 
@@ -80,23 +81,23 @@ while(True):
     else: #opcion incorrecta
         print(msg.err)
 
-opcion1 = -1 #opcion1 = opciones del menu principal
+opcion1 = -1 #opcion1 = opciones del menu principal--------------------------------------
 
 while(True):
     print(msg.seleccion)
     print(msg.menu_principal)
     opcion1 = int(input())
     
-    if(opcion1 == 0): #Salir del programa
+    if(opcion1 == 0): #Salir del programa --------------------------------------
         print(msg.gracias) 
         break
-    
+    #----------------------------------------------------------------------------
     elif(opcion1 == 1):#primera opcion agregar datos ficticios
         agregarDatosFicticios()
         Ficticios.datos_desde_txt("ficticios.txt",lista_clientes) #agrega clientes ficticios desde txt
         print(msg.datosFicticios)
-        
-    elif(opcion1 == 2): #segunda opcion menu principal #ingreso propietario
+    #----------------------------------------------------------------------------
+    elif(opcion1 == 2): #segunda opcion menu principal ingreso propietario
         print(msg.ingreso)
         print(msg.in_cedula)
         ced = int(input())
@@ -108,6 +109,7 @@ while(True):
             print(msg.bienv_usuario+logeado.getNombre()) #Ingreso exitoso como propietario
 
             opcion2 = -1  #opcion2 = opciones del menu funcionario
+            #Menu propietario----------------------------------------------------------------------------
             while(True):  # Ingreso al menu funcionario
                 print(msg.menu_propietario)
                 opcion2 = int(input())
@@ -194,13 +196,14 @@ while(True):
 
                 elif(opcion2 == 7): #opcion 7 menu propietario Aprobar arriendos
                     print("aprobar arriendos")
-
+            #Fin menu propietario----------------------------------------------------------------------------
 
                     
-
+        #----------------------------------------------------------------------------
         else:
             print(msg.err_datos) #Datos erroneos funcionario
-                
+   
+    #----------------------------------------------------------------------------           
     elif(opcion1 == 3): #opcion 3 menu principal registrtar cliente
         print(msg.registro)
         print(msg.in_cedula)
@@ -240,7 +243,7 @@ while(True):
 
                 else: #No ingresa 1 o 2
                     print(msg.err)
-
+    #--------------------------------------------------------
     elif(opcion1 == 4):  #opciop 4 del menu principal Ingreso Como Cliente
         print(msg.ingreso)
 
@@ -254,7 +257,8 @@ while(True):
             print(msg.bienv_usuario+logeado.getNombre()) #Ingreso exitoso como cliente
 
             opciones_cliente = -1  #opcion2 = opciones del menu cliente
-            while(True):  # Ingreso al menu cliente
+            #Menu cliente--------------------------------------------------------
+            while(True):  
                 print(msg.seleccion)
                 print(msg.menu_cliente)
                 opciones_cliente = int(input())
@@ -264,12 +268,49 @@ while(True):
 
                 elif(opciones_cliente == 1):  #Mostrar arriendos disponibles
                     print(msg.arriendos_disp)
-                    Arriendo.mostrarArriendos(lista_arriendos)
+                    Arriendo.arriendosDisponibles(lista_arriendos)
                 
                 elif(opciones_cliente == 2):  #Mostrar compraventas disponibles
                     print(msg.compraventas_disp)
-                    Compraventa.mostrarCompraventas(lista_compraventas)
-            
+                    Compraventa.compraventasDisponibles(lista_compraventas)
+
+                elif(opciones_cliente == 3):  #Seleccionar compraventa
+                    print(msg.sel_compraventa)
+                    codigo_compraventa=int(input())
+                    compraventa_actual=Compraventa.buscarCompraventa(lista_compraventas,codigo_compraventa)
+                    if compraventa_actual==None or not compraventa_actual.getDisponible():
+                        print("Debe ingresar un codigo de compraventa valido")
+                    else:
+                        print(compraventa_actual.__str__())
+                        oferta=input("Desea aplicar a esta compraventa:(s|n)")
+                        if oferta=="s":
+                            compraventa_actual.setDisponible(False)
+                            compraventa_actual.setComprador(logeado)#enlace entre compraventa y el comprador
+                            logeado.addContrato(compraventa_actual)
+                            print("aplico")
+                        else:
+                            print("No aplico a la compraventa de cogigo: "+str(compraventa_actual.getCodigo()))
+
+                elif(opciones_cliente == 4): #Seleccionar arriendo
+                    print(msg.sel_arriendo)
+                    codigo_arriendo=int(input())
+                    arriendo_actual=Arriendo.buscarArriendo(lista_arriendos,codigo_arriendo)
+                    
+                    if arriendo_actual==None or not arriendo_actual.getDisponible():
+                        print("Debe ingresar un codigo de arriendo valido")
+                    else:
+                        print(arriendo_actual.__str__())
+                        oferta=input("Desea aplicar a este arriendo:(s|n)")
+                        if oferta=="s":
+                            arriendo_actual.setDisponible(False)
+                            arriendo_actual.setArrendatario(logeado)#enlace entre arriendo y el arrendatario
+                            logeado.addContrato(arriendo_actual)#se añade el contrato de arrendamieno a la lista de contratos del cliente
+                            print("Aplico")
+                        else:
+                            print("No aplico al arriendo de codigo "+str(arriendo_actual.getCodigo()))
+
+                elif(opciones_cliente == 5):#ver mis contratos actuales
+                    print(Contrato.mostrarValoresContratos(logeado.getContratos()))
         else:
             print(msg.err_datos) #Datos erroneos cliente
             
